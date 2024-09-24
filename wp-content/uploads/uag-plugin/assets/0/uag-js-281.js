dfbcf2bac3735b7ed1683986133a944df2483959
Kaushal <ganatrakaushal.dance@.com>
2024-09-23 17:14:18 -0700

    Modify site to v2

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-281.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-b84af7dc', {"layout":"number","heading":"Placements every year","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":70,"totalNumber":100,"decimalPlaces":0,"animationDuration":1501,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-2fa9954c', {"layout":"number","heading":"Candidates Placed","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":175,"totalNumber":175,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-14ef0b0a', {"layout":"number","heading":"Happy Customers","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":312,"totalNumber":400,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-dcd05062', {"layout":"number","heading":"Years of Experience","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":2,"totalNumber":175,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
